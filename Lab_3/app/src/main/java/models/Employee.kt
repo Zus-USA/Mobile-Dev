bc75b110a27785549537101bb25cb3e5f299bb65
Zus-USA <nanfull@mail.ru>
2026-03-20 20:00:56 +1100

    5

--- Lab_3/app/src/main/java/models/Employee.kt
+++
@@ -1,8 +0,0 @@
-package models
-
-data class Employee(
-    val name: String,
-    val department: String,
-    val salary: Double,
-    val experience: Int
-)
--- Lab_3/app/src/main/java/models/Product.kt
+++
@@ -1,8 +0,0 @@
-package models
-
-data class Product(
-    val name: String,
-    val category: String,
-    val price: Double,
-    val inStock: Boolean
-)
